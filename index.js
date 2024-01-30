2d005026576c79db4c0075676ebd2265550eaf41
mathew-at-guelph <136002781+mathew-at-guelph@users.noreply.github.com>
2024-01-30 15:12:39 -0500

    Update index.js
    Deleted unused codes.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import util from "util"; // Import the 'util' module for Promise support
+import util from "util";
 import { exec } from "child_process";
 import multer from "multer";
 import { fileURLToPath } from 'url';
@@ -51,56 +51,3 @@ app.post("/new", upload.single("excelFile"), async (req, res) => {
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
 });
-
-// import express from "express";
-// import dotenv from "dotenv";
-// import { exec } from "child_process";
-// import multer from "multer";
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-// import path from 'path';
-// import fs from 'fs';
-// dotenv.config();
-
-// const PORT = process.env.PORT || 8080;
-// const app = express();
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// const storage = multer.diskStorage({
-//   destination: function(req, file, cb) {
-//     cb(null, path.join(__dirname, "uploads"));
-//   },
-//   filename: function(req, file, cb) {
-//     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post("/new", upload.single("excelFile"), (req, res) => {
-//   const uploadedFile = req.file;
-
-//   if (!uploadedFile) {
-//     return res.status(400).json({ error: "No file uploaded" });
-//   }
-//   const filePath = path.join(__dirname, "uploads", uploadedFile.filename);
-//   const command = `./parser.bin parse oca -p ${filePath}`;
-
-//   exec(command, (error, stdout, stderr) => {
-//     if (error) {
-//       return res.status(500).json({ error: "Internal Server Error" });
-//     }
-//     try {
-//       const parsedData = JSON.parse(stdout);
-//       fs.unlinkSync(filePath);
-//       res.json({ schema: parsedData });
-//     } catch (parseError) {
-//       return res.status(500).json({ error: "Error parsing JSON" });
-//     }
-//   });
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`App running on ${PORT}`);
-// });
